8d5e4555891aadc0d5e616c561c6e2bf549fd50f
Kyriakos Ioulianou <kyriakos@Kyriakoss-MacBook-Air.local>
2017-10-28 14:48:56 +0200

    I'm too good malaka

--- colleen/colleen.c
+++ colleen/colleen.c
@@ -1,3 +1,4 @@
+/*outside comment*/
 #include <stdio.h>
-char*s="#include <stdio.h>%cchar*s=%c%s%c;%cint main(void){printf(s,10,34,s,34,10,10);}%c";
-int main(void){printf(s,10,34,s,34,10,10);}
+char*s="/*outside comment*/%c#include <stdio.h>%cchar*s=%c%s%c;%cint main(void){printf(s,10,10,34,s,34,10,10);/*inside comment*/}%c";
+int main(void){printf(s,10,10,34,s,34,10,10);/*inside comment*/}
--- grace/grace.c
+++ grace/grace.c
@@ -1,3 +1,9 @@
 #define fun main
+#define Zir z
+#define Upo u
 #include <unistd.h>
-char *s="#define fun main
+#include <fcntl.h>
+#include <stdio.h>
+char *s="#define fun main%c#define Zir z%c#define Upo u%c#include <unistd.h>%c#include <fcntl.h>%c#include <stdio.h>%cchar *s=%c%s%c;%cint fun(){int fd = open(%cgrace_kid.c%c, O_CREAT|O_RDWR, 0666);%cdprintf(fd,s,10,10,10,10,10,10,34,s,34,10,34,34,10,10);}%c";
+int fun(){int fd = open("grace_kid.c", O_CREAT|O_RDWR, 0666);
+dprintf(fd,s,10,10,10,10,10,10,34,s,34,10,34,34,10,10);}
